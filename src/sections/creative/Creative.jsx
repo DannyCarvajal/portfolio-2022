019b823c2a9d93c3d01802bf050f880c05c5675c
Oliver Daniel <juan-daniel1914@hotmail.com>
2021-11-08 15:31:07 -0500

    additional queries for sections, tablet and horizontal view

--- src/sections/creative/Creative.jsx
+++ src/sections/creative/Creative.jsx
@@ -1,4 +1,3 @@
-import {useState, useEffect, useRef} from "react";
 import Piano from "../../components/creative/piano";
 import usePianoLogic from "../../hooks/pianoLogic";
 import SecretWord from "../../components/global/secretword";
