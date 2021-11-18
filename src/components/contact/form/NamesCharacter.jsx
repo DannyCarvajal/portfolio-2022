326194542aeea2cf74aa6e8ae4eacb70ff03d156
Oliver Daniel <juan-daniel1914@hotmail.com>
2021-11-17 23:07:25 -0500

    random character on names form functionality

--- src/components/contact/form/NamesCharacter.jsx
+++ src/components/contact/form/NamesCharacter.jsx
@@ -1,11 +1,53 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import {female, male} from "./names";
 
 import "./namesCharacter.scss";
 
-const NamesCharacter = () => {
-	const properNames = ["sergio", "max", "lewis"];
-	let randNumber = Math.floor(Math.random() * 6 + 1);
-	const [currentCharacter, setCurrentCharacter] = useState(`f${randNumber}`);
+const NamesCharacter = ({name}) => {
+	// IAMGES SAVED BY A SPECIFIC NAME
+	const maleNames = ["sergio", "max", "lewis"];
+	const femaleNames = [];
+
+	// HELPFUL FUNCTIONS
+	let randNumber = () => Math.floor(Math.random() * 6 + 1);
+	const findGender = () => {
+		if (name.trim() !== "" && female.includes(name + " ")) {
+			return "female";
+		} else if (name.trim() !== "" && male.includes(name + " ")) {
+			return "male";
+		} else {
+			return "unknown";
+		}
+	};
+	// STATE
+	const [currentCharacter, setCurrentCharacter] = useState(`o${randNumber()}`);
+
+	useEffect(() => {
+		let gender = findGender();
+		console.log(name);
+
+		if (femaleNames.includes(name)) {
+			setCurrentCharacter(`${name}`);
+			console.log("female and no photo");
+			return;
+		}
+
+		if (maleNames.includes(name)) {
+			setCurrentCharacter(`${name}`);
+			console.log("male and no photo");
+			return;
+		}
+
+		if (gender === "female") {
+			setCurrentCharacter(`f${randNumber()}`);
+			console.log("female");
+		}
+
+		if (gender === "male") {
+			setCurrentCharacter(`m${randNumber()}`);
+			console.log("male");
+		}
+	}, [name]);
 
 	return (
 		<div className="namesCharacter">
